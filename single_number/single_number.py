90be8d69fa91cd2d04298710c2f0e9a91410d97f
jose-marquez89 <jlmarquez89@gmail.com>
2020-06-17 17:54:34 -0500

    identifies single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,18 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+
+def single_number(arr):
+    # first pass solution
+    counts = [0] * (max(arr) + 1)
+    for i in arr:
+        counts[i] += 1
+
+    for j in range(len(counts)):
+        if counts[j] == 1:
+            return j
+    return None
 
 
 if __name__ == '__main__':
