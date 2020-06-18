1365e3b5f0c90a5c118dc9a9506097f2f5761706
jose-marquez89 <jlmarquez89@gmail.com>
2020-06-18 14:10:28 -0500

    completes first pass solution

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,14 +1,41 @@
+from pdb import set_trace as bp
+import logging
+
+FORMAT = "%(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+logging.disable(logging.DEBUG)
 '''
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+
+def eating_cookies(n):
+    if n == 0:
+        return 1
+    upper_list = ['3'] * n
+    upper = int(''.join(upper_list)) + 1
+    valid_ways = []
+    counter = 0
+    logging.debug(f"UPPER: {upper}")
+
+    for i in range(1, upper):
+        nums = [int(x) for x in str(i) if int(x) > 0 and int(x) < 4]
+        if nums in valid_ways:
+            del nums
+            continue
+        logging.debug(f"NUMS {nums}")
+        total = sum(nums)
+        if total == n:
+            counter += 1
+            valid_ways.append(nums)
+            del nums
+
+    return counter
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 10
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
